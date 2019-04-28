7d91a8023dce74f81e3981e48546745d62b5e521
CSTDev <pietomb00@gmail.com>
2019-04-28 12:28:38 +0100

    Implement sms.SendMessage method

--- pkg/notification/sms.go
+++ pkg/notification/sms.go
@@ -1,10 +1,29 @@
 package notification
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sns"
+	"github.com/aws/aws-sdk-go/service/sns/snsiface"
 	log "github.com/sirupsen/logrus"
 )
 
-func SendMessage(message string, number string) bool {
+type SMS struct {
+	Client snsiface.SNSAPI
+}
+
+func (s *SMS) SendMessage(message string, number string) error {
 	log.Info("Sending message")
-	return false
+	messageParams := &sns.PublishInput{
+		Message:     aws.String(message),
+		PhoneNumber: aws.String(number),
+	}
+	resp, err := s.Client.Publish(messageParams)
+	if err != nil {
+		log.Error("Failed to send text message")
+		return err
+	}
+
+	log.Info(resp)
+
+	return nil
 }
